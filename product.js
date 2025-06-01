fc3222882ac389c975708d7ed02ca71f228daaab
Priyanka-18-stack <priyabef@gmail.com>
2025-06-01 16:41:44 +0530

    updated

---
+++ product.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+        id:1,
+        name:"Blue dress",
+        src:"products/f7 (1).jpg",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+    {
+        id:2,
+        name:"Satin frock Green",
+        src:"products/f2.jpg",
+        desc:"",
+        price:260,
+        tags:["new","green","party"]
+    },
+
+    {
+        id:3,
+        name:"Green Shirt",
+        src:"products/f9.jpg",
+        desc:"",
+        price:399,
+        tags:["old","green","summer"]
+    },
+
+    {
+        id:4,
+        name:"Blue lace shirt",
+        src:"products/f4.jpg",
+        desc:"",
+        price:399,
+        tags:["old","white","party"]
+    },
+    {
+        id:5,
+        name:"Beach Shirt",
+        src:"pexels-ogproductionz-17243661.jpg",
+        desc:"",
+        price:579,
+        tags:["old","white","beach"]
+    }
+,
+    {
+        id:6,
+        name:"Shirt Party Red",
+        src:"wmremove-transformed (1).jpeg",
+        desc:"",
+        price:579,
+        tags:["new","red","party"]
+    },
+
+    {
+        id:7,
+        name:"Lavender",
+        src:"photo-1559034750-cdab70a66b8e.avif",
+        desc:"",
+        price:399,
+        tags:["new","white","party"]
+    },
+    {
+        id:8,
+        name:"Maxi-match",
+        src:"pexels-muhammadendry-32327073.jpg",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+    {
+        id:9,
+        name:"Crop-top",
+        src:"pexels-ogproductionz-17243573.jpg",
+        desc:"",
+        price:220,
+        tags:["old","white","summer"]
+    },
+
+
+]
+
+export {products}
